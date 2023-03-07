b05658a35e2c76a8df773741da9b4697e2a599e0
Jeffrey Chau <jeffreyd.chau@gmail.com>
2023-03-07 14:53:19 -0800

    starting fen

--- generator.py
+++ generator.py
@@ -22,7 +22,7 @@ def bestMove(board, move_count):
 
 def twoPieceGen(board):
     print("results will always equal draw for two piece board")
-    print("Storing: \n"+ "Draw" + "\n" + str(board.fen()))
+    print("Storing: \n" + "Starting fen: " + str(starting_fen) + "\nw/d/l: "+ "Draw" + "\nEnding fen: " + str(board.fen()))
     
 
 def threePieceGen(board, move_count):
@@ -39,26 +39,26 @@ def threePieceGen(board, move_count):
             print("Black wins!" + " in " + str(move_count) + "moves")
             winner="Black win"
         print("Endgame board state:\n" + str(board))
-        print("Storing: \n" + winner + "\n" + str(board.fen()))
+        print("Storing: \n" + "Starting fen: " + str(starting_fen) + "\nw/d/l: "+ winner + "\nEnding fen: " + str(board.fen()))
     elif board.can_claim_threefold_repetition():
         print("Repeated move move 3 times. Therefore draw")
-        print("Storing: \n"+ "Draw" + "\n" + str(board.fen()))
+        print("Storing: \n" + "Starting fen: " + str(starting_fen) + "\nw/d/l: "+ "Draw" + "\nEnding fen: " + str(board.fen()))
     elif board.is_fifty_moves():
         print("No change in board state in 50 turns. Therefore draw")
-        print("Storing: \n"+ "Draw" + "\n" + str(board.fen()))
+        print("Storing: \n" + "Starting fen: " + str(starting_fen) + "\nw/d/l: "+ "Draw" + "\nEnding fen: " + str(board.fen()))
     else:
         bestMove(board, move_count)
 
 ## --------------------------------------Start READING HERE -----------------------------------------
 #Diffrent board states to test
 #KRvK
-#board = chess.Board("5k2/8/8/4R1K1/8/8/8/8 w - - 0 1")
+board = chess.Board("5k2/8/8/4R1K1/8/8/8/8 w - - 0 1")
 
 #KPvK
 #board = chess.Board("8/8/P1K3k1/8/8/8/8/8 w - - 0 1")
 
 #KQvK
-board = chess.Board("8/2Q5/6k1/8/3K4/8/8/8 w - - 0 1")
+#board = chess.Board("8/2Q5/6k1/8/3K4/8/8/8 w - - 0 1")
 
 #KvK
 #board = chess.Board("4k3/8/8/4K3/8/8/8/8 b - - 0 1")
@@ -75,7 +75,7 @@ board = chess.Board("8/2Q5/6k1/8/3K4/8/8/8 w - - 0 1")
 
 print("Total number of pieces on the board:", len(board.piece_map()))
 print("Starting board: \n" + str(board))
-
+starting_fen = board.fen()
 
 #Two piece generator
 if len(board.piece_map()) ==2:
@@ -88,10 +88,10 @@ if len(board.piece_map()) ==3:
     print(fenBoard)
     if fenBoard.count("b") > 1 or fenBoard.count("B") >= 1 :
         print("Bishop and king cannot checkmate. Therefore draw")
-        print("Storing: \n"+ "Draw" + "\n" + str(board.fen()))
+        print("Storing: \n" + "Starting fen: " + str(starting_fen) + "\nw/d/l: "+ "Draw" + "\nEnding fen: " + str(board.fen()))
     elif fenBoard.__contains__('n') or fenBoard.__contains__('N'):
         print("Knight and king cannot checkmate. Therefore draw")
-        print("Storing: \n"+ "Draw" + "\n" + str(board.fen()))
+        print("Storing: \n" + "Starting fen: " + str(starting_fen) + "\nw/d/l: "+ "Draw" + "\nEnding fen: " + str(board.fen()))
     else:
         threePieceGen(board, 0)
     
